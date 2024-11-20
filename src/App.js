9f931bb9560d0d27f05f9cb72145792267769102
rwireprathamesh570 <prathamesh.patil@researchwire.in>
2024-11-20 18:58:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import RecordList from "./components/RecordList";
 import RecordForm from "./components/RecordForm";
@@ -11,12 +11,25 @@ const App = () => {
     <Router>
       <Navbar bg="dark" variant="dark" expand="lg" className="mb-4 sticky-top">
         <Container>
-          <Navbar.Brand href="/">MERN User Records</Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className="text-white text-decoration-none fw-bold">
+              MERN User Records
+            </Link>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/create">Create Record</Nav.Link>
+              <Nav.Link>
+                <Link to="/" className="text-white text-decoration-none">
+                  Home
+                </Link>
+              </Nav.Link>
+              <Nav.Link>
+                {" "}
+                <Link to="/create" className="text-white text-decoration-none">
+                  Add New
+                </Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
